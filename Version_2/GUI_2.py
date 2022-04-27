d91aa498504487555ab68aa069a19c41ba8958df
unknown <robert.constable@analyte.uk>
2022-04-27 21:02:35 +0100

    message

--- Version_2/GUI_2.py
+++ Version_2/GUI_2.py
@@ -26,11 +26,11 @@ logo = ImageTk.PhotoImage(logo)
 canvas.create_image(15, 150, image=logo)
 
 User_input = tk.Label(canvas, text="Postcode:", font=("Raleway","8"))
-User_input.pack(padx=20, pady=50)
+User_input.pack(padx=20, pady=150)
 
 postcode_input = tk.Entry(canvas, highlightthickness='0.5',width='10')
 postcode_input.configure(highlightbackground='red')
-postcode_input.pack(padx=40,pady=90)
+postcode_input.pack(padx=20,pady=(90)
 
 User_input_url = tk.Label(canvas,text='URL', font=('raleway', '8'))
 User_input_url.pack(padx=100,pady=90)
@@ -40,7 +40,5 @@ right_move.pack(padx=20,pady=80)
 
 quit = tk.Button(canvas, text='Quit', command=quit_app)
 quit.pack(padx=100,pady=100)
-
-
 
 root.mainloop()
